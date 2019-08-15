40c62d4c0c817676143ef664b0a5bc32a97e46fb
kwanghyunMoon <mgh3326@naver.com>
2019-08-15 20:41:00 +0900

    Initial commit

---
+++ src/main/java/me/khmoon/shlibraryapi/ShLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package me.khmoon.shlibraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShLibraryApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShLibraryApiApplication.class, args);
+  }
+
+}
